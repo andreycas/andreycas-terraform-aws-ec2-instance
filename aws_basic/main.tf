abe5c6c8716f0a1ce36fec5140e59610eade6e7c
Andreycas <60447336+andreycas@users.noreply.github.com>
2020-06-18 12:37:44 -0500

    Update main.tf

--- aws_basic/main.tf
+++ aws_basic/main.tf
@@ -2,7 +2,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 //variable "aws_ami" {default = "ami-0080e4c5bc078760e"}
 variable "aws_ami" {default = "ami-031ecc7e9d5b9a82d"}
-variable "aws_security_group_id" {default = "sg-8d35f2f5"}
+variable "aws_security_group_id" {default = "sg-0e797008e6f3e9fad"}
 variable "instance_type" {default = "t2.micro"}
 
 provider "aws" {
@@ -14,6 +14,6 @@ provider "aws" {
 resource "aws_instance" "cda_instance" {
   ami                    = "${var.aws_ami}"
   instance_type          = "${var.instance_type}"
-  vpc_security_group_ids = ["${var.aws_security_group_id}"]
+  vpc_security_group_ids = ["${var.aws_security_group_id}"]  
   key_name	             = "johnny-key-us-east-1"
 }
