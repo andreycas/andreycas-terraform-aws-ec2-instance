8c1de6e025d5228c78b69cce6aeaa169fd5b523e
Andreycas <jhonnyandrey27@gmail.com>
2020-06-19 11:19:25 -0500

    changes
    changes

--- johnny_aws/main.tf
+++ johnny_aws/main.tf
@@ -48,7 +48,7 @@ resource "aws_instance" "cda_instance" {
 
 resource "aws_ses_template" "MyTemplateJ" {
 	name    = "MyTemplateJ_${local.in_id}"
-	subject = "Greetings, Johnny!"
+	subject = "Greetings, Jeny!"
 	html    = "<h1>Hello johnny,</h1><p>Your app url is http://${aws_instance.cda_instance.*.public_ip[0]}.</p>"
 	text    = "Hello johnny, Your app url is http://${aws_instance.cda_instance.*.public_ip[0]}."
 }
