e80ecf661e82401e2636ebe4ea9fbba37c6c0f03
Andreycas <60447336+andreycas@users.noreply.github.com>
2020-06-24 17:32:08 -0500

    Update main.tf

--- johnny_aws/main.tf
+++ johnny_aws/main.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "cda_instance" {
 //	vpc_security_group_ids = "default"
 //	key_name	       = "${var.aws_key_name}"
 //	key_name	= "jeny-key-us-east-1"
-        key_name	= "terraform_keys"	
+        key_name	= "terraform_keys2"	
 
 	user_data = <<HEREDOC
 		#!/bin/bash
