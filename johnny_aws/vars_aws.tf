a0ba501769dc62c1d1ca43d2b36c653358026af7
Andreycas <60447336+andreycas@users.noreply.github.com>
2020-06-24 17:32:42 -0500

    Update vars_aws.tf

--- johnny_aws/vars_aws.tf
+++ johnny_aws/vars_aws.tf
@@ -7,11 +7,11 @@ variable "aws_secret_key" {
 }
 
 variable "aws_key_name" {
-	default = "terraform_keys"
+	default = "terraform_keys2"
 }
 
 variable "private_key_file" {
-	default = "terraform_keys.ppk"
+	default = "terraform_keys2.pem"
 }
 variable "aws_ami" {
 	default = "ami-0080e4c5bc078760e"
